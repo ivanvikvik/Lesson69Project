86cbd785594d66d8e47c40b9e41be397ad6bda98
velfor <velfor@tut.by>
2023-05-30 18:44:11 +0300

    Change subclasses изменение дочерних классов

--- Lesson69Project/main.cpp
+++ Lesson69Project/main.cpp
@@ -1,23 +1,26 @@
 #include "gasStation.h"
+#include "car.h"
+#include "bus.h"
+#include "truck.h"
 
 int main(void) {
-	Car car1(50, 200, 1600);
-	Car car2(60, 240, 2000);
-	Car car3(40, 160, 1200);
+	Car car1();
+	Car* car2 = new Car();
 
-	Bus bus(200, 50);
+	Transport* transport = new Transport();
 
-	Truck truck1(300, 20000);
-	Truck truck2(350, 25000);
+	delete transport;
 
-	Car cars[3] = { car1, car2, car3 };
-	Bus buses[1] = { bus };
-	Truck trucks[2] = { truck1, truck2 };
-
-	int total = GasStation::calculateTotalGas(buses, 1, trucks, 2,
-		cars, 3);
-
-	cout << "Total fuel is " << total << endl;
+	transport = new Car();
+	delete transport;
+	
+	transport = new Bus();
+	delete transport;
+	
+	transport = new Truck();
+	
+	
+	//cout << "Total fuel is " << total << endl;
 
 	return 0;
 }
